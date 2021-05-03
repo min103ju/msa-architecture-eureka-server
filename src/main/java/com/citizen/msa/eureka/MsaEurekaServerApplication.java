4b96992615ee43182e0b2d4c20ed59b527e05c16
Min Heo <min103ju@gmail.com>
2021-05-03 22:08:39 +0900

    Update application.yml

--- src/main/java/com/citizen/msa/eureka/MsaEurekaServerApplication.java
+++ src/main/java/com/citizen/msa/eureka/MsaEurekaServerApplication.java
@@ -7,10 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class MsaEurekaServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MsaEurekaServerApplication.class, args);
-
-        git remote add origin https://github.com/min103ju/msa-architecture-eureka-server.git
     }
 }
